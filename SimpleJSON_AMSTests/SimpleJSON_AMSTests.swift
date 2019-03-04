1565260daecde8f4991e292c1f9884176d5e3472
Adam Saxton <ams17a@acu.edu>
2019-03-04 08:18:04 -0600

    Initial Commit

---
+++ SimpleJSON_AMSTests/SimpleJSON_AMSTests.swift
@@ -0,0 +1,34 @@
+//
+//  SimpleJSON_AMSTests.swift
+//  SimpleJSON_AMSTests
+//
+//  Created by Adam Saxton on 3/4/19.
+//  Copyright © 2019 Adam Saxton. All rights reserved.
+//
+
+import XCTest
+@testable import SimpleJSON_AMS
+
+class SimpleJSON_AMSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
